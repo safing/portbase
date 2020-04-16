30a6948009045dc16da78ad7c8e2eae1bb53e064
Daniel <dhaavi@users.noreply.github.com>
2020-04-16 13:13:40 +0200

    Remove config and use service workers for goroutines

--- rng/rng_test.go
+++ rng/rng_test.go
@@ -2,17 +2,10 @@ package rng
 
 import (
 	"testing"
-
-	"github.com/safing/portbase/config"
 )
 
 func init() {
-	err := prep()
-	if err != nil {
-		panic(err)
-	}
-
-	err = Start()
+	err := start()
 	if err != nil {
 		panic(err)
 	}
@@ -21,25 +14,17 @@ func init() {
 func TestRNG(t *testing.T) {
 	key := make([]byte, 16)
 
-	err := config.SetConfigOption("random/rng_cipher", "aes")
-	if err != nil {
-		t.Errorf("failed to set random/rng_cipher config: %s", err)
-	}
-	_, err = newCipher(key)
+	rngCipher = "aes"
+	_, err := newCipher(key)
 	if err != nil {
 		t.Errorf("failed to create aes cipher: %s", err)
 	}
-	rng.Reseed(key)
 
-	err = config.SetConfigOption("random/rng_cipher", "serpent")
-	if err != nil {
-		t.Errorf("failed to set random/rng_cipher config: %s", err)
-	}
+	rngCipher = "serpent"
 	_, err = newCipher(key)
 	if err != nil {
 		t.Errorf("failed to create serpent cipher: %s", err)
 	}
-	rng.Reseed(key)
 
 	b := make([]byte, 32)
 	_, err = Read(b)
@@ -54,5 +39,10 @@ func TestRNG(t *testing.T) {
 	_, err = Bytes(32)
 	if err != nil {
 		t.Errorf("Bytes failed: %s", err)
+	}
+
+	_, err = Number(100)
+	if err != nil {
+		t.Errorf("Number failed: %s", err)
 	}
 }
