2b07a6900e1ca969a45b01458972af2c27528b96
Daniel <dhaavi@users.noreply.github.com>
2021-04-03 14:14:26 +0200

    Rename FlagController to BroadcastFlag

--- utils/flag.go
+++ utils/flag.go
@@ -6,79 +6,79 @@ import (
 	"github.com/tevino/abool"
 )
 
-// FlagController is a simple system to broadcast a flag value.
-type FlagController struct {
+// BroadcastFlag is a simple system to broadcast a flag value.
+type BroadcastFlag struct {
 	flag   *abool.AtomicBool
 	signal chan struct{}
 	lock   sync.Mutex
 }
 
-// Flag receives changes from its FlagController.
+// Flag receives changes from its broadcasting flag.
 // A Flag must only be used in one goroutine and is not concurrency safe,
 // but fast.
 type Flag struct {
-	flag       *abool.AtomicBool
-	signal     chan struct{}
-	controller *FlagController
+	flag        *abool.AtomicBool
+	signal      chan struct{}
+	broadcaster *BroadcastFlag
 }
 
-// NewFlagController returns a new FlagController.
+// NewBroadcastFlag returns a new BroadcastFlag.
 // In the initial state, the flag is not set and the singal does not trigger.
-func NewFlagController() *FlagController {
-	return &FlagController{
+func NewBroadcastFlag() *BroadcastFlag {
+	return &BroadcastFlag{
 		flag:   abool.New(),
 		signal: make(chan struct{}),
 		lock:   sync.Mutex{},
 	}
 }
 
-// NewFlag returns a new Flag controlled by this controller.
+// NewFlag returns a new Flag that listens to this broadcasting flag.
 // In the initial state, the flag is set and the singal triggers.
 // You can call Refresh immediately to get the current state from the
-// controller.
-func (cfc *FlagController) NewFlag() *Flag {
+// broadcasting flag.
+func (bf *BroadcastFlag) NewFlag() *Flag {
 	newFlag := &Flag{
-		flag:       abool.NewBool(true),
-		signal:     make(chan struct{}),
-		controller: cfc,
+		flag:        abool.NewBool(true),
+		signal:      make(chan struct{}),
+		broadcaster: bf,
 	}
 	close(newFlag.signal)
 	return newFlag
 }
 
-// NotifyAndReset notifies all flags of this controller and resets the
-// controller state.
-func (cfc *FlagController) NotifyAndReset() {
-	cfc.lock.Lock()
-	defer cfc.lock.Unlock()
+// NotifyAndReset notifies all flags of this broadcasting flag and resets the
+// internal broadcast flag state.
+func (bf *BroadcastFlag) NotifyAndReset() {
+	bf.lock.Lock()
+	defer bf.lock.Unlock()
 
 	// Notify all flags of the change.
-	cfc.flag.Set()
-	close(cfc.signal)
+	bf.flag.Set()
+	close(bf.signal)
 
 	// Reset
-	cfc.flag = abool.New()
-	cfc.signal = make(chan struct{})
+	bf.flag = abool.New()
+	bf.signal = make(chan struct{})
 }
 
 // Signal returns a channel that waits for the flag to be set. This does not
 // reset the Flag itself, you'll need to call Refresh for that.
-func (cf *Flag) Signal() <-chan struct{} {
-	return cf.signal
+func (f *Flag) Signal() <-chan struct{} {
+	return f.signal
 }
 
 // IsSet returns whether the flag was set since the last Refresh.
 // This does not reset the Flag itself, you'll need to call Refresh for that.
-func (cf *Flag) IsSet() bool {
-	return cf.flag.IsSet()
+func (f *Flag) IsSet() bool {
+	return f.flag.IsSet()
 }
 
-// Refresh fetches the current state from the controller.
-func (cf *Flag) Refresh() {
-	cf.controller.lock.Lock()
-	defer cf.controller.lock.Unlock()
+// Refresh fetches the current state from the broadcasting flag.
+func (f *Flag) Refresh() {
+	f.broadcaster.lock.Lock()
+	defer f.broadcaster.lock.Unlock()
 
-	// Copy current flag and signal from the controller.
-	cf.flag = cf.controller.flag
-	cf.signal = cf.controller.signal
+	// Copy current flag and signal from the broadcasting flag.
+	f.flag = f.broadcaster.flag
+	f.signal = f.broadcaster.signal
 }
