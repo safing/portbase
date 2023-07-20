52ba3f0a156ba4576e10d455296c9578557916af
Daniel Hovie <dhaavi@users.noreply.github.com>
2023-07-20 14:24:14 +0200

    Update option.go
    Improve naming

--- config/option.go
+++ config/option.go
@@ -108,11 +108,10 @@ const (
 	// requirement. The type of RequiresAnnotation is []ValueRequirement
 	// or ValueRequirement.
 	RequiresAnnotation = "safing/portbase:config:requires"
-
-	// SettingRequiresFeaturePlan can be used to mark a setting as only available
-	// when the user has a certain feature ID activated by the subscription plan.
+	// RequiresFeaturePlan can be used to mark a setting as only available
+	// when the user has a certain feature ID in the subscription plan.
 	// The type is []string or string.
-	SettingRequiresFeaturePlan = "safing/portmaster:ui:config:requires-feature"
+	RequiresFeatureID = "safing/portmaster:ui:config:requires-feature"
 )
 
 // QuickSettingsAction defines the action of a quick setting.
